74b34063127fdd14d86104f898ff3dd9330f05db
Rodrigo Ledesma Pinet <50053346+roipinet@users.noreply.github.com>
2022-07-22 07:21:51 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+function goToSection(newSection, button) {
+  // Look for the active section
+  var activeSection = document.querySelector(".section.active");
+  var activeButton = document.querySelector(".navbar__link.active");
+  // Only make a change if newSection is different
+  if (activeSection.id !== newSection) {
+    activeSection.classList.remove("active");
+    activeButton.classList.remove("active");
+    document.getElementById(newSection).classList.add("active");
+    document.getElementById(button).classList.add("active");
+  }
+}
+
+function openServiceSection(newServiceSection) {
+  // Look for the active service section
+  var activeServiceSection = document.querySelector(".service-section.active");
+  console.info("active service: ", activeServiceSection.classList);
+  // Only make a change if newSection is different
+  if (!activeServiceSection.classList.contains(newServiceSection)) {
+    console.info(
+      "!activeServiceSection.classList.contains(",
+      newServiceSection,
+      ")"
+    );
+    activeServiceSection.classList.remove("active");
+    document.querySelector(newServiceSection).classList.add("active");
+  }
+  document.querySelector(".service-section.active").scrollIntoView();
+}
